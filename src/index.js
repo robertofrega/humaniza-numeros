99377640ac485119ac5ce013b420bbe6e77df1c3
R.Frega <robertofrega@gmail.com>
2016-12-02 14:50:06 -0200

    feat(method): add method that converts long numbers to their compact version
    covers all current tests

--- src/index.js
+++ src/index.js
@@ -1,5 +1,20 @@
-function humanizaNumeros(input, decimals = 0) {
+const escalaCurta = [
+  '', //n*10^0*3
+  'Mil', //n*10^1*3
+  'Milhão', //n*^2*3
+  'Bilhão', //...
+  'Trilhão',
+  'Quadrilhão',
+  'Quintilhão'
+];
 
+function humanizaNumeros(input, decimals = 0) {
+  const indexEscala = Math.ceil(input.toString().length / 3) - 1;
+  const divisor = Math.pow(10, 3 * indexEscala);
+  const base = input / divisor;
+  const escala = base <= 2 ? escalaCurta[indexEscala] : escalaCurta[indexEscala].replace('ão', 'ões');
+
+  return `${base.toFixed(decimals).replace('.', ',')} ${escala}`;
 }
 
 export default humanizaNumeros;
